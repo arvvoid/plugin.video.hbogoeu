cc75e0b6c1f2fa8a357d3208a7aee37cb6f2512f
Fodor David <scottanyo@gmail.com>
2017-11-17 00:35:42 +0100

    operatorok, settings fix

--- addon.py
+++ addon.py
@@ -46,16 +46,31 @@ search_string = urllib.unquote_plus(__settings__.getSetting('lastsearch'))
 operator = __settings__.getSetting('operator')
 op_ids = [
 '00000000-0000-0000-0000-000000000000', # Anonymous NoAuthenticated
-'15276cb7-7f53-432a-8ed5-a32038614bbf', # HBO GO 
-'48f48c5b-e9e4-4fca-833b-2fa26fb1ad22', # UPC Dire
-
+'15276cb7-7f53-432a-8ed5-a32038614bbf', # HBO GO webes
+'48f48c5b-e9e4-4fca-833b-2fa26fb1ad22', # UPC Direct
+'b7728684-13d5-46d9-a9a4-97d676cdaeec', # DIGI
+'04459649-8a90-46f1-9390-0cd5b1958a5d', # Magyar Telekom Nyrt.
+'e71fabae-66b6-4972-9823-8743f8fcf06f', # Telenor MyTV
+'1ca45800-464a-4e9c-8f15-8d822ad7d8a1', # UPC Magyarország
+'f2230905-8e25-4245-80f9-fccf67a24005', # INVITEL
+'383cd446-06fb-4a59-8d39-200a3e9bcf6f', # Celldömölki Kábeltelevízió Kft.
+'fe106c75-293b-42e6-b211-c7446835b548', # Eurocable – Hello Digital
+'42677aa5-7576-4dc7-9004-347b279e4e5d', # HFC-Network Kft.
+'3a3cce31-fb19-470a-9bb5-6947c4ac9996', # HIR-SAT 2000 Kft.
+'c6441ec8-e30f-44b6-837a-beb2eb971395', # Jurop Telekom
+'d91341c2-3542-40d4-adab-40b644798327', # Kabelszat 2002
+'18fb0ff5-9cfa-4042-be00-638c5d34e553', # Klapka Lakásszövetkezet
+'97cddb59-79e3-4090-be03-89a6ae06f5ec', # Lát-Sat Kft.
+'c48c350f-a9db-4eb6-97a6-9b659e2db47f', # MinDig TV Extra
+'7982d5c7-63df-431d-806e-54f98fdfa36a', # PARISAT
+'18f536a3-ecac-42f1-91f1-2bbc3e6cfe81', # PR-TELECOM
+'adb99277-3899-439e-8bdf-c749c90493cd', # TARR Kft
+'5729f013-f01d-4cc3-b048-fe5c91c64296', # Vác Városi Kábeltelevízió Kft.
+'b4f422f7-5424-4116-b72d-7cede85ead4e', # Vidanet Zrt.
+'6a52efe0-54c4-4197-8c55-86ee7a63cd04', # HBO Development Hungary
+'f320aa2c-e40e-49c2-8cdd-1ebef2ac6f26', # HBO GO Vip/Club Hungary
 ]
 op_id = op_ids[int(operator)];
-
-
-
-username = __settings__.getSetting('username')
-password = __settings__.getSetting('password')
 
 individualization = ""
 goToken = ""
@@ -137,6 +152,8 @@ def LOGIN():
 	global loggedin_headers
 
 
+	username = __settings__.getSetting('username')
+	password = __settings__.getSetting('password')
 	customerId = __settings__.getSetting('customerId')
 	individualization = __settings__.getSetting('individualization')
 
