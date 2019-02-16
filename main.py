ad8cd0814998d1a47840a8654bcb98faa5a07163
arvvoid <arv.void@gmail.com>
2019-02-16 16:48:00 +0100

    Guided initial setup v1, work in progress

--- main.py
+++ main.py
@@ -15,35 +15,11 @@ BASE_URL = sys.argv[0]
 # We use string slicing to trim the leading ? from the plugin call paramstring
 REQUEST_PARAMS = sys.argv[2][1:]
 
-def get_addon_data():
-    # Loads the Kodi plugin data from addon.xml
-    root_dir = os.path.dirname(os.path.abspath(__file__))
-    pathname = os.path.join(root_dir, 'addon.xml')
-    with open(pathname, 'rb') as addon_xml:
-        addon_xml_contents = addon_xml.read()
-        _id = re.search(
-            r'(?<!xml )id="(.+?)"',
-            addon_xml_contents).group(1)
-        author = re.search(
-            r'(?<!xml )provider-name="(.+?)"',
-            addon_xml_contents).group(1)
-        name = re.search(
-            r'(?<!xml )name="(.+?)"',
-            addon_xml_contents).group(1)
-        version = re.search(
-            r'(?<!xml )version="(.+?)"',
-            addon_xml_contents).group(1)
-        return {
-            'id': _id,
-            'author': author,
-            'name': name,
-            'version': version
-        }
-
 
 if __name__ == '__main__':
-    addon_data = get_addon_data()
-    xbmc.log(addon_data['id'] + " STARING VERSION: " + addon_data['version'], xbmc.LOGDEBUG)
-    addon_main = hbogo(addon_data.id, PLUGIN_HANDLE, BASE_URL)
+    id = "plugin.video.hbogoeu"
+    version = "2.0.3~beta9"
+    xbmc.log(id + " STARING VERSION: " + version, xbmc.LOGDEBUG)
+    addon_main = hbogo(id, PLUGIN_HANDLE, BASE_URL)
     addon_main.router(REQUEST_PARAMS)
 
