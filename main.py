4a2cc980a22dcc6787bf0275eb10eb03666396a5
Arv.Void <46710439+arvvoid@users.noreply.github.com>
2019-05-09 15:29:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # encoding: utf-8
-# base handler class for hbogo Kodi add-on
+# Hbo Go Kodi Add-on start
 # Copyright (C) 2019 ArvVoid (https://github.com/arvvoid)
 # Relesed under GPL version 2
 #########################################################
