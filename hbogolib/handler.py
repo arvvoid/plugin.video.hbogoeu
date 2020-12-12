2f92e623831fa333584e29c159bfb1de66f4d7a1
Arv.Void <arv.void@gmail.com>
2020-12-12 11:39:14 +0100

    Sub cache auto maintenance, minor style fixes, revision bump (#169)
    * Autoclean subtitles cache and clean subtitle cache option
    
    * Revision bump
    
    * Code style fixes

--- hbogolib/handler.py
+++ hbogolib/handler.py
@@ -553,6 +553,15 @@ class HbogoHandler(object):
             xbmcplugin.addDirectoryItem(handle=self.handle, url=tmp_url, listitem=liz, isFolder=True)
         KodiUtil.endDir(self.handle, None, True)
 
+    def clean_sub_cache(self, silent=True):
+        import shutil
+        subs_folder = xbmc.translatePath(self.addon.getAddonInfo('profile'))
+        subs_folder = subs_folder + 'subs'
+        shutil.rmtree(subs_folder, ignore_errors=True)
+        if not silent:
+            icon = self.get_resource("icon.png")
+            xbmcgui.Dialog().notification(self.language(30726), self.LB_INFO, icon)
+
     # IMPLEMENT THESE IN SPECIFIC REGIONAL HANDLER
 
     def setup(self, country, forceeng=False):
