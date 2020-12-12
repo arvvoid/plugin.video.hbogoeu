2475d620b5bf3ba2f8a3882d7984424670a11df5
arvvoid <arv.void@gmail.com>
2020-12-12 11:29:29 +0100

    Style fix for import translatePath

--- libs/kodiutil.py
+++ libs/kodiutil.py
@@ -11,16 +11,16 @@ from kodi_six import xbmcplugin  # type: ignore
 
 # import for translate path
 if sys.version_info < (3, 0):  # for Kodi 18 use old translatePath
-    from kodi_six import xbmc as kodipath  # type: ignore
+    from kodi_six.xbmc import translatePath  # type: ignore
 else:  # for Kodi 19+ use new translatePath
-    from kodi_six import xbmcvfs as kodipath  # type: ignore
+    from kodi_six.xbmcvfs import translatePath  # type: ignore
 
 
 class KodiUtil(object):
 
     @staticmethod
     def translatePath(path):
-        return kodipath.translatePath(path)
+        return translatePath(path)
 
     @staticmethod
     def addSorting(handle, use_content_type):
